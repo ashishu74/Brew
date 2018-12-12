40a7912fd344640d7490d5e39591b71434f2f459
Ashish Kumar <ashishkumar@ucreate.co.in>
2018-12-12 17:47:04 +0530

    changes code chrome options

--- src/test/java/utility/BrowserFactory.java
+++ src/test/java/utility/BrowserFactory.java
@@ -18,8 +18,10 @@ public class BrowserFactory {
 		}
 		else if(browserName.equals("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
-			driver=(WebDriver) new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox")
 		}		
 		driver.manage().window().maximize();
 		driver.get(url);
