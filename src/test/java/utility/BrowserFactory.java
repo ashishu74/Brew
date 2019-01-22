7e8990770083862cb2c7885565955b4c390b7b04
ashish kumar <ashishkumar@ucreate.co.in>
2019-01-22 10:53:28 +0530

    chrome options

--- src/test/java/utility/BrowserFactory.java
+++ src/test/java/utility/BrowserFactory.java
@@ -1,7 +1,7 @@
 package utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
