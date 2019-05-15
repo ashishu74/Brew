46e8330f2e21cae021f874be2e7b72a3fbf600c3
ashish kumar <ashishkumar@ucreate.co.in>
2019-05-15 17:13:07 +0530

    browser factory changes

--- src/test/java/utility/BrowserFactory.java
+++ src/test/java/utility/BrowserFactory.java
@@ -12,29 +12,19 @@ public class BrowserFactory {
 	public static WebDriver startBrowser(String browserName,String url) throws InterruptedException{
 		if(browserName.equals("firefox"))
 		{
-			System.setProperty("webdriver.gecko.driver", "/home/ucreate-66/eclipse-workspace/brew/Drivers/geckodriver");
+			System.setProperty("webdriver.gecko.driver", "");
 			driver=(WebDriver) new FirefoxDriver();
 		}
 		else if(browserName.equals("chrome"))
 		{
-<<<<<<< HEAD
-			System.setProperty("webdriver.chrome.driver", "/home/ucreate-66/eclipse-workspace/brew/Drivers/chromedriver");
-			/*
-=======
 			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 			
->>>>>>> a67b8f5397566c18f219b324a8e7421038d20ce1
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("headless");
 			options.addArguments("window-size=1400,800");
 		
-<<<<<<< HEAD
-			driver = (WebDriver) new ChromeDriver(options);Thread.sleep(5000);*/
-			driver=(WebDriver) new ChromeDriver();
-=======
 			driver = (WebDriver) new ChromeDriver(options);Thread.sleep(5000);
-			//driver=(WebDriver) new ChromeDriver();
->>>>>>> a67b8f5397566c18f219b324a8e7421038d20ce1
+			driver=(WebDriver) new ChromeDriver();
 		}		
 		driver.manage().window().maximize();
 		driver.get(url);
